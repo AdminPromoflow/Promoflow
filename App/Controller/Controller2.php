19bb3c4b318b9721c573318e8f01d8e6dd748825
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2023-08-04 15:37:52 -0500

    m

--- App/Controller/Controller2.php
+++ App/Controller/Controller2.php
@@ -1,33 +1,18 @@
 <?php
 session_start();
+
+/*---------------------------------  Imports  --------------------------------*/
+
 require_once('../Config/database.php');
 require_once('../Models/Users.php');
 require_once('../Models/Categories.php');
 require_once('../pest-master/PestJSON.php');
 include ('../Data/flapi_credentials.php');
 
-    if ($_POST['module']=="updateUsers") {
-      $db = new Database();
-      $user = new Users($db);
-      $user->setIdUser($_POST['idUser']);
-      $user->setName($_POST['name']);
-      $user->setEmail($_POST['email']);
-      $user->setPassword($_POST['password']);
-      $user->setUserType($_POST['userType']);
-      $result = json_encode($user->updateUser());
-      echo $result;
-    }
-    elseif ($_POST['module']=="loginUser") {
-      $db = new Database();
-      $user = new Users($db);
-      $user->setEmail($_POST['email']);
-      $user->setPassword($_POST['password']);
-      $result = json_encode($user->readUserExist());
-      echo $result;
-      $_SESSION['loginUser'] = 'active';
-    }
-    elseif ($_POST['module']=="createUser") {
 
+/*--------------------------------  CRUD users  ------------------------------*/
+
+    if ($_POST['module']=="createUser") {
       $db = new Database();
       $user = new Users($db);
       $user->setEmail($_POST['email']);
@@ -43,10 +28,21 @@ include ('../Data/flapi_credentials.php');
         $result = json_encode($user->createUser());
       }
       echo $countUsers;
-
     }
-    elseif ($_POST['module']=="deleteUser") {
 
+    elseif ($_POST['module']=="updateUsers") {
+      $db = new Database();
+      $user = new Users($db);
+      $user->setIdUser($_POST['idUser']);
+      $user->setName($_POST['name']);
+      $user->setEmail($_POST['email']);
+      $user->setPassword($_POST['password']);
+      $user->setUserType($_POST['userType']);
+      $result = json_encode($user->updateUser());
+      echo $result;
+    }
+
+    elseif ($_POST['module']=="deleteUser") {
       $db = new Database();
       $user = new Users($db);
       $user->setIdUser($_POST['idUser']);
@@ -54,91 +50,85 @@ include ('../Data/flapi_credentials.php');
       echo $result;
     }
 
-      elseif ($_POST['module']=="verifyLogin") {
-        if ($_SESSION['loginUser'] == 'active') {
-          echo json_encode(1);
-        }
-        else {
-          echo json_encode(0);
-        }
-      }
 
-      elseif ($_POST['module']=="logout") {
-        session_destroy();
-      }
+/*----------------------------------  Login  ---------------------------------*/
 
-      elseif ($_POST['module']=="requestProductsExample") {
-      }
-
-      elseif ($_POST['module']=="getOrders") {
-      //  $url = 'https://NKA1.API:46fKgJsZP7@dev-7.flyerlink.com/api.php/runs?state=waiting&days=60&prod_workgrp=NKA1&format=json';
-
-      // Init the library (put in your TC credentials here)
-          $apiClient = new PestJSON('https://dev-7.flyerlink.com/api.php');
-          //  $apiClient->setupAuth( 'username', 'password' );
-          $apiClient->setupAuth($flapi[0],$flapi[1]);
-
-          // Make a simple get call (this returns tag types in JSON format)
-          try {
-              $result = $apiClient->get('/runs?state=waiting&days=120&format=json');
-          }
-          catch (Exception $e) {
-              // Handle error. In practice there are several exception types,
-              // which you can use to differentiate between different error conditions
-              die( $e->getMessage());
-          }
-          // Examine successful result
-
-          echo(json_encode( $result) );
-
-
-
-
-      //  $json = file_get_contents('../../Json/GETRunsAwaitingProduction.json');
-        // Display data
-      //  echo ($json);
+    elseif ($_POST['module']=="loginUser") {
+      $db = new Database();
+      $user = new Users($db);
+      $user->setEmail($_POST['email']);
+      $user->setPassword($_POST['password']);
+      $result = json_encode($user->readUserExist());
+      echo $result;
+      $_SESSION['loginUser'] = 'active';
     }
 
-      elseif ($_POST['module']=="getOrdersContent") {
-        $apiClient = new PestJSON('https://dev-7.flyerlink.com/api.php');
-        //  $apiClient->setupAuth( 'username', 'password' );
-        $apiClient->setupAuth($flapi[0],$flapi[1]);
+    elseif ($_POST['module']=="verifyLogin") {
+      if ($_SESSION['loginUser'] == 'active') {
+        echo json_encode(1);
+      }
+      else {
+        echo json_encode(0);
+      }
+    }
 
-        // Make a simple get call (this returns tag types in JSON format)
-        try {
-            $result = $apiClient->get('/runs/'.$_POST['idOrder'].'/jobs?format=json');
+    elseif ($_POST['module']=="logout") {
+      session_destroy();
+    }
+
+
+/*----------------------------------  Orders  --------------------------------*/
+
+    elseif ($_POST['module']=="getOrders") {
+        $apiClient = new PestJSON('https://dev-7.flyerlink.com/api.php'); // Init the library (put in your TC credentials here)
+        $apiClient->setupAuth($flapi[0],$flapi[1]); //  $apiClient->setupAuth( 'username', 'password' );
+
+        try { // Make a simple get call (this returns tag types in JSON format)
+            $result = $apiClient->get('/runs?state=waiting&days=120&format=json');
         }
-        catch (Exception $e) {
-            // Handle error. In practice there are several exception types,
-            // which you can use to differentiate between different error conditions
-            die( $e->getMessage());
+        catch (Exception $e) { // Handle error. In practice there are several exception types,
+            die( $e->getMessage()); // which you can use to differentiate between different error conditions
         }
-        // Examine successful result
 
-        echo(json_encode( $result) );
+        echo(json_encode( $result) ); // Examine successful result
 
-        //$url =  'https://NKA1.API:46fKgJsZP7@dev-7.flyerlink.com/api.php/runs/'.$_POST['idOrder'].'/jobs?format=json';
-      //  $json = file_get_contents('../../Json/GETRUNJobs.json');
-        // Display data
-      //  echo ($json);
+    }
+
+    elseif ($_POST['module']=="getOrdersContent") {
+      $apiClient = new PestJSON('https://dev-7.flyerlink.com/api.php');
+      $apiClient->setupAuth($flapi[0],$flapi[1]);
+
+      try {
+          $result = $apiClient->get('/runs/'.$_POST['idOrder'].'/jobs?format=json');
       }
-      elseif ($_POST['module']=="getCustomers") {
-        //$url =  'https://NKA1.API:46fKgJsZP7@dev-7.flyerlink.com/api.php/runs/'.$_POST['idOrder'].'/jobs?format=json';
-        $json = file_get_contents('../../Json/Customer.json');
-        // Display data
-        echo ($json);
-      }
-      elseif ($_POST['module']=="getCategories") {
-        $db = new Database();
-        $user = new Categories($db);
-        $result = json_encode($user->getCategories());
-        echo $result;
+      catch (Exception $e) {
+          die( $e->getMessage());
       }
 
-      elseif ($_POST['module']=="getGroups") {
-        echo $_POST['id'];
-      }
+      echo(json_encode( $result) );
+    }
 
+
+/*---------------------------------  Products  -------------------------------*/
+
+    elseif ($_POST['module']=="getCategories") {
+      $db = new Database();
+      $user = new Categories($db);
+      $result = json_encode($user->getCategories());
+      echo $result;
+    }
+
+    elseif ($_POST['module']=="getGroups") {
+      echo $_POST['id'];
+    }
+
+
+/*---------------------------------  Customers  ------------------------------*/
+
+    elseif ($_POST['module']=="getCustomers") {
+      $json = file_get_contents('../../Json/Customer.json');
+      echo ($json);
+    }
       //
 
 
