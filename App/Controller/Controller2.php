e3ada320ac0fd1e103891b19288f616659aba655
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2023-08-11 06:22:21 -0500

    Update Controller2.php

--- App/Controller/Controller2.php
+++ App/Controller/Controller2.php
@@ -57,12 +57,12 @@ require_once('../Models/Users.php');
 
     elseif ($_POST['module']=="loginUser") {
       $db = new Database();
-      $user = new Users($db);
+      /*$user = new Users($db);
       $user->setEmail($_POST['email']);
       $user->setPassword($_POST['password']);
       $result = json_encode($user->readUserExist());
       echo $result;
-      $_SESSION['loginUser'] = 'active';
+      $_SESSION['loginUser'] = 'active';*/
     }
 
     /*elseif ($_POST['module']=="verifyLogin") {
