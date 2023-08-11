4ef27572db149e4477fd9d1f41c0832c53757b90
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2023-08-11 06:27:43 -0500

    Update Controller2.php

--- App/Controller/Controller2.php
+++ App/Controller/Controller2.php
@@ -56,13 +56,13 @@ include ('../Data/flapi_credentials.php');
 /*----------------------------------  Login  ---------------------------------*/
 
      if ($_POST['module']=="loginUser") {
-      /*$db = new Database();
+      $db = new Database();
       $user = new Users($db);
       $user->setEmail($_POST['email']);
       $user->setPassword($_POST['password']);
       $result = json_encode($user->readUserExist());
       echo $result;
-      $_SESSION['loginUser'] = 'active';*/
+      $_SESSION['loginUser'] = 'active';
     }
 
     /*elseif ($_POST['module']=="verifyLogin") {
