4c448e371ac9f52fb27a6c2ac45dbfb27b834eb9
AdminPromoflow <137961323+AdminPromoflow@users.noreply.github.com>
2023-08-11 06:26:43 -0500

    Update Controller2.php

--- App/Controller/Controller2.php
+++ App/Controller/Controller2.php
@@ -1,10 +1,10 @@
 <?php
-//session_start();
+session_start();
 
 /*---------------------------------  Imports  --------------------------------*/
 
-//require_once('../Config/database.php');
-//require_once('../Models/Users.php');
+require_once('../Config/database.php');
+require_once('../Models/Users.php');
 //require_once('../Models/Categories.php');
 //require_once('../Models/Groups.php');
 //require_once('../Models/Products.php');
