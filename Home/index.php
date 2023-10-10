e61ca81efc384917462c880a9650f8b9539d40d5
aelnajard <aelnajard@gmail.com>
2023-10-10 17:48:10 -0500

    “…”

--- Home/index.php
+++ Home/index.php
@@ -334,7 +334,7 @@
             password: data[1].value
           },
           success: function(data){
-            alert(data);
+          //  alert(data);
 
             var data = jQuery.parseJSON(data);
         //    alert(data["COUNT(*)"]);
