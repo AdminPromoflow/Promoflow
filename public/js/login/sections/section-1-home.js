8d4418beb27e2b14d17c1247c887fbf65567ff27
aelnajard <aelnajard@gmail.com>
2023-10-13 17:09:39 -0500

    “…”

--- public/js/login/sections/section-1-home.js
+++ public/js/login/sections/section-1-home.js
@@ -10,11 +10,12 @@ class Login {
       password: passwordLogin.value  // Get password value from the second element in the 'vars' array
     };
 
-    alert(emailLogin.value + "  " + passwordLogin.value);
 
     // Initialize event listeners for the login button and password input field
     access.addEventListener("click", function () {
       if (loginClass.validateEmail() && loginClass.validatePassword()) {
+        alert(emailLogin.value + "  " + passwordLogin.value);
+
         loginClass.makeAjaxRequestLogin(url, data);
       }
     });
