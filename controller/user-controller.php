e6f1b0a1a26041f47dec7fa45c043341525339b1
aelnajard <aelnajard@gmail.com>
2023-10-13 16:56:56 -0500

    “…”

--- controller/user-controller.php
+++ controller/user-controller.php
@@ -8,7 +8,7 @@ class ApiHandler {
             $rawData = file_get_contents("php://input");
 
             $data = json_decode($rawData);
-            echo json_encode($data);exit;
+            echo json_encode($data->password);exit;
 
 
             // Check if JSON data is valid and contains an "action" field
