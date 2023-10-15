0e029f4e0a222334aceb06282b90565abb828d25
aelnajard <aelnajard@gmail.com>
2023-10-15 16:17:35 -0500

    “…”

--- controller/user-controller.php
+++ controller/user-controller.php
@@ -107,7 +107,7 @@ class ApiHandler {
                 $response = array("message" => "Login successful");
                 echo
             } else {
-                $response = array("message" => false);
+                $response = array("message" => 'false');
                 echo json_encode($response); // Usar un array en json_encode
             }
         } else {
