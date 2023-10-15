84ac22c4452f34d2f3a59a7b321d17991bbb62f2
aelnajard <aelnajard@gmail.com>
2023-10-15 12:08:16 -0500

    “…”

--- controller/user-controller.php
+++ controller/user-controller.php
@@ -102,7 +102,7 @@ class ApiHandler {
             $storedHash = $user->getPasswordUserByEmail();
             $password = $data->password;
 
-            echo json_encode("string");
+            echo json_encode($data);
 
             if (password_verify($password, $storedHash)) {
                 $response = array("message" => "Login successful");
