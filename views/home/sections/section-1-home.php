99017bfe470861b09634406273efd40419e502dd
aelnajard <aelnajard@gmail.com>
2023-10-15 22:01:41 -0500

    “…”

--- views/home/sections/section-1-home.php
+++ views/home/sections/section-1-home.php
@@ -1,23 +1,30 @@
 <section class="bodyLogin">
+  <!-- Section Title -->
   <h1 class="titleBodyLogin">Dashboard</h1>
 
+  <!-- Welcome Message -->
   <h2>Welcome! Please select one of the following options:</h2>
+
+  <!-- Container for User Options -->
   <div class="containerUsersManagerBottoms">
 
+    <!-- User Option 1 -->
     <div class="itemUsersManager">
       <img src="../../public/img/home/sections/section-1-home/W3P.png" alt="">
       <h3>W3P</h3>
     </div>
 
+    <!-- User Option 2 -->
     <div class="itemUsersManager">
       <img src="../../public/img/home/sections/section-1-home/Amazon.png" alt="">
       <h3>AMAZON</h3>
     </div>
 
+    <!-- User Option 3 -->
     <div class="itemUsersManager">
       <img src="../../public/img/home/sections/section-1-home/LFY.png" alt="">
       <h3>LANYARDS FOR YOU</h3>
     </div>
 
-    </div>
+  </div>
 </section>
