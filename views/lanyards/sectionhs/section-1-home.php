54907f66d3d264f27db47683877f90d23668fc3d
aelnajard <aelnajard@gmail.com>
2023-10-16 12:44:11 -0500

    “…”

--- views/lanyards/sectionhs/section-1-home.php
+++ views/lanyards/sectionhs/section-1-home.php
@@ -1,9 +1,9 @@
 <section class="bodyLogin">
   <!-- Section Title -->
-  <h1 class="titleBodyLogin">Dashboard</h1>
+  <h1 class="titleBodyLogin">Lanyards</h1>
 
   <!-- Welcome Message -->
-  <h2>Welcome! Please select one of the following options:</h2>
+  <h2>Select one of the following options:</h2>
 
   <!-- Container for User Options -->
   <div class="containerUsersManagerBottoms">
@@ -13,7 +13,7 @@
     <!-- User Option 3 -->
     <div class="itemUsersManager">
       <img src="../../public/img/home/sections/section-1-home/LFY.png" alt="">
-      <h3>LANYARDS FOR YOU*****</h3>
+      <h3>Customers</h3>
     </div>
 
   </div>
