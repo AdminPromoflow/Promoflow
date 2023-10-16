787c9bace4366d73a489d6c8d683f99d574d91c5
aelnajard <aelnajard@gmail.com>
2023-10-15 19:46:58 -0500

    “…”

--- views/home/sections/section-1-home.php
+++ views/home/sections/section-1-home.php
@@ -4,18 +4,15 @@
 
     <div class="itemUsersManager">
       <img src="../../public/img/home/sections/section-1-home/W3P.png" alt="">
-      <h3>W3P</h3>
     </div>
 
     <div class="itemUsersManager">
       <img src="../../public/img/home/sections/section-1-home/Amazon.png" alt="">
-      <h3>AMAZON</h3>
     </div>
 
     <div class="itemUsersManager">
       <img src="../../public/img/home/sections/section-1-home/LFY.png" alt="">
-      <h3>LANYARDS FOR YOU</h3>
     </div>
-    
+
     </div>
 </section>
