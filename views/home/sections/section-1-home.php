1f15ce471a4180bccf1345ff891b614bf36199b7
aelnajard <aelnajard@gmail.com>
2023-10-16 09:33:18 -0500

    “…”

--- views/home/sections/section-1-home.php
+++ views/home/sections/section-1-home.php
@@ -9,22 +9,25 @@
   <div class="containerUsersManagerBottoms">
 
     <!-- User Option 1 -->
+    <a href="#">
     <div class="itemUsersManager">
       <img src="../../public/img/home/sections/section-1-home/W3P.png" alt="">
       <h3>W3P</h3>
     </div>
-
+    </a>
     <!-- User Option 2 -->
+    <a href="#">
     <div class="itemUsersManager">
       <img src="../../public/img/home/sections/section-1-home/Amazon.png" alt="">
       <h3>AMAZON</h3>
     </div>
-
+    </a>
     <!-- User Option 3 -->
+    <a href="../../">
     <div class="itemUsersManager">
       <img src="../../public/img/home/sections/section-1-home/LFY.png" alt="">
       <h3>LANYARDS FOR YOU</h3>
     </div>
-
+    </a>
   </div>
 </section>
---
+++ views/lanyards/sectionhs/section-1-home.php
@@ -0,0 +1,20 @@
+<section class="bodyLogin">
+  <!-- Section Title -->
+  <h1 class="titleBodyLogin">Dashboard</h1>
+
+  <!-- Welcome Message -->
+  <h2>Welcome! Please select one of the following options:</h2>
+
+  <!-- Container for User Options -->
+  <div class="containerUsersManagerBottoms">
+
+
+
+    <!-- User Option 3 -->
+    <div class="itemUsersManager">
+      <img src="../../public/img/home/sections/section-1-home/LFY.png" alt="">
+      <h3>LANYARDS FOR YOU*****</h3>
+    </div>
+
+  </div>
+</section>
