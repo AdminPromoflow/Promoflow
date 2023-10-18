66448727f1abb6e4883a285f8d9918ca4047eb55
aelnajard <aelnajard@gmail.com>
2023-10-17 23:59:34 -0500

    “…”

--- views/lanyards/lanyards-customers/sections/section-1-home.php
+++ views/lanyards/lanyards-customers/sections/section-1-home.php
@@ -2,17 +2,20 @@
   <!-- Section Title -->
   <h1 class="titleBodyLogin">Lanyard Customers</h1>
 
-  <div class="customerBox">
-    <div class="imgCustomer">
-      <div class="circleImgCustomer">
+  <div id="listCustomers">
+    <div class="customerBox">
+      <div class="imgCustomer">
+        <div class="circleImgCustomer">
 
+        </div>
+      </div>
+      <div class="nameCustomer">
+        <p>Alejandra Paola Rozo Chiquiza</p>
+      </div>
+      <div class="emailCustomer">
+        <p>aelnajard@gmail.com</p>
       </div>
     </div>
-    <div class="nameCustomer">
-      <p>Alejandra Paola Rozo Chiquiza</p>
-    </div>
-    <div class="emailCustomer">
-      <p>aelnajard@gmail.com</p>
-    </div>
   </div>
+
 </section>
