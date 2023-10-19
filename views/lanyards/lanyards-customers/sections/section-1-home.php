14e08781e855fef8cf431e03584289a0b590902a
aelnajard <aelnajard@gmail.com>
2023-10-19 11:52:05 -0500

    “…”

--- views/lanyards/lanyards-customers/sections/section-1-home.php
+++ views/lanyards/lanyards-customers/sections/section-1-home.php
@@ -4,46 +4,6 @@
 
   <div id="listCustomers">
 
-    <div class="customerBox">
-      <div class="imgCustomer">
-        <div class="circleImgCustomer">
-
-        </div>
-      </div>
-      <div class="nameCustomer">
-        <p>Alejandra Paola Rozo Chiquiza</p>
-      </div>
-      <div class="emailCustomer">
-        <p>aelnajard@gmail.com</p>
-      </div>
-    </div>
-
-    <div class="customerBox">
-      <div class="imgCustomer">
-        <div class="circleImgCustomer">
-        </div>
-      </div>
-      <div class="nameCustomer">
-        <p>Alejandra Paola Rozo Chiquiza</p>
-      </div>
-      <div class="emailCustomer">
-        <p>aelnajard@gmail.com</p>
-      </div>
-    </div>
-
-    <div class="customerBox">
-      <div class="imgCustomer">
-        <div class="circleImgCustomer">
-        </div>
-      </div>
-      <div class="nameCustomer">
-        <p>Alejandra Paola Rozo Chiquiza</p>
-      </div>
-      <div class="emailCustomer">
-        <p>aelnajard@gmail.com</p>
-      </div>
-    </div>
-
   </div>
 
 </section>
