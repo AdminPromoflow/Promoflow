464554d4af8dd910c355a171d01b1141081be3ad
aelnajard <aelnajard@gmail.com>
2023-10-19 11:33:12 -0500

    “…”

--- assets/js/lanyards/lanyards-customers/sections/section-1-home.js
+++ assets/js/lanyards/lanyards-customers/sections/section-1-home.js
@@ -43,16 +43,21 @@ class LanyardCustomers {
       listCustomers.innerHTML =+
 
       '<div class="customerBox">'+
+
         '<div class="imgCustomer">'+
           '<div class="circleImgCustomer">'+
+
           '</div>'+
         '</div>'+
+
         '<div class="nameCustomer">'+
           '<p>Alejandra Paola Rozo Chiquiza</p>'+
         '</div>'+
+
         '<div class="emailCustomer">'+
           '<p>aelnajard@gmail.com</p>'+
         '</div>'+
+
       '</div>';
 
 
