d81db5b11f42d0986a9abb75a55a343b64cf0d5f
aelnajard <aelnajard@gmail.com>
2023-12-19 13:12:14 -0500

    “…”

--- views/W3P/index.php
+++ views/W3P/index.php
@@ -2,7 +2,20 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Promoflow</title>
+
+    <!-- Preconnect and link to Google Fonts for Montserrat and Roboto -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500&family=Roboto:wght@100;300;400&display=swap" rel="stylesheet">
+
+    <!-- Set the viewport for responsive design -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- Include CSS files for styling -->
+    <link rel="stylesheet" href="../../assets/css/global/menu.css">
+    <link rel="stylesheet" href="../../assets/css/home/style.css">
+
   </head>
   <body>
     <!-- Include the menu and section components using PHP include -->
