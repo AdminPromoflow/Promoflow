38f8da6e5169602aeeb18f56710406d9d9c522bd
aelnajard <aelnajard@gmail.com>
2023-12-20 23:12:31 -0500

    “…”

--- views/W3P_products/sections/section-1-home.php
+++ views/W3P_products/sections/section-1-home.php
@@ -1,8 +1,20 @@
+<div class="navigateTable">
+  <img id="leftArrowW3P" src="Images/arrow.png" alt="">
+  <div class="textNavigatorTable">
+    <h3>Products</h3>
+  </div>
+  <div class="textNavigatorTable">
+    <h3>Orders</h3>
+  </div>
+  <div class="textNavigatorTable">
+    <h3>ToSendPO</h3>
+  </div>
 
-<section class="bodyLogin">
-  <h2 class="titleBodyLogin">Products</h2>
+  <img id="rightArrowW3P" src="Images/arrow.png" alt="">
+</div>
+<div class="bodyLogin">
   <div class="containerTableW3P">
     <div id="containersItemsCategory"  class="containerCenterTable">
     </div>
   </div>
-</section>
+</div>
