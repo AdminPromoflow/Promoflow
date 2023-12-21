77fcf6116b8eca4ff6e271c9d87a4492b0a50088
aelnajard <aelnajard@gmail.com>
2023-12-20 23:39:05 -0500

    “…”

--- views/W3P_products/sections/section-1-home.php
+++ views/W3P_products/sections/section-1-home.php
@@ -7,6 +7,8 @@
   <img id="rightArrowW3P" src="../../assets/img/W3P_products/sections/section-1-home/arrow.png" alt="">
 </div>
 <div class="bodyLogin">
+  <!-- Welcome Message -->
+  <h2>Please expand the categories and groups to see the products available in our system.:</h2>
   <div class="containerTableW3P">
     <div id="containersItemsCategory"  class="containerCenterTable">
     </div>
