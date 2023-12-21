e27030a70bca102ac60099a58a48cd367e19d866
aelnajard <aelnajard@gmail.com>
2023-12-21 00:12:15 -0500

    “…”

--- views/W3P_products/sections/section-1-home.php
+++ views/W3P_products/sections/section-1-home.php
@@ -1,10 +1,11 @@
 <div class="navigateTable">
-  <img id="leftArrowW3P" src="../../assets/img/W3P_products/sections/section-1-home/arrow.png" alt="">
+  <a href="#"><img id="" src="../../assets/img/W3P_products/sections/section-1-home/arrow.png" alt=""></a>
+
   <div class="textNavigatorTable">
     <h3>Products</h3>
   </div>
+  <a href="#"><img id="" src="../../assets/img/W3P_products/sections/section-1-home/arrow.png" alt=""></a>
 
-  <img id="rightArrowW3P" src="../../assets/img/W3P_products/sections/section-1-home/arrow.png" alt="">
 </div>
 <div class="bodyLogin">
   <!-- Welcome Message -->
