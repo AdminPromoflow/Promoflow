121edc8b56c8dffba1e1e0778da3a062931c7010
aelnajard <aelnajard@gmail.com>
2023-12-20 22:11:10 -0500

    “…”

--- controller/W3P/W3P_products/products.php
+++ controller/W3P/W3P_products/products.php
@@ -6,10 +6,10 @@ require_once('../../../models/W3P/W3P_products/Products.php');
 
 
     if ($_POST['module']=="getCategories") {
-      $db = new Database();
+    /*  $db = new Database();
       $category = new Categories($db);
       $result = json_encode($category->getCategories());
-      echo $result;
+      echo $result;*/
     }
 
   /*  elseif ($_POST['module']=="getGroups") {
