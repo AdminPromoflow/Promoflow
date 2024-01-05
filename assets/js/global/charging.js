996f79a636ca66fba4f34ffee2d8561c1dd6b8e5
aelnajard <aelnajard@gmail.com>
2024-01-04 22:31:02 -0500

    “…”

--- assets/js/global/charging.js
+++ assets/js/global/charging.js
@@ -5,7 +5,19 @@
 var progressBarCharging = document.getElementById("progressBarCharging");
 
 
-function chargeProgressBar(percentCharged){
-  progressBarCharging.style.width = percentCharged +  "%";
+function chargeProgressBar(finalPercent){
+  let presentPercent = 0;
+
+  const interval =  setInterval(() => {
+    presentPercent ++;
+    progressBarCharging.style.width = percentCharged +  "%";
+
+    if (presentPercent  >=  finalPercent) {
+      clearInterval(interval);
+    }
+
+
+  },30)
+
 }
 chargeProgressBar(50);
