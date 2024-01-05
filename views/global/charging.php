93d99a4f8558c57cb2da8e0a532127f298c8cba5
aelnajard <aelnajard@gmail.com>
2024-01-04 21:12:27 -0500

    “…”

--- views/global/charging.php
+++ views/global/charging.php
@@ -3,30 +3,3 @@
     <img src="../../assets/img/global/charging/logo.png" alt="">
   </div>
 </section>
-<style media="screen">
-.charging{
-  position: fixed;
-  width: 100vw;
-  height: 100vh;
-  top: 0px;
-  left: 0px;
-  background-color: rgba(0, 0, 0, .5);
-
-
-}
-.iconCharging{
-  position: absolute;
-  height: 80px;
-  width: 120px;
-  left: 50%;
-  top: 50%;
-  z-index: 100;
-  transform: translate(-50%, -50%);
-  background-color: rgba(255, 255, 255, 1);
-}
-.iconCharging img{
-  position: relative;
-  width: 100%;
-}
-
-</style>
