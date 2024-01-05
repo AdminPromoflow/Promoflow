5cf2b93e9f0a8b307ed7e5b4e3293c32bbb67677
aelnajard <aelnajard@gmail.com>
2024-01-04 22:45:59 -0500

    “…”

--- assets/js/W3P/sections/section-1-home.js
+++ assets/js/W3P/sections/section-1-home.js
@@ -1,2 +1,5 @@
 // Create an instance of the Menu class to handle menu functionality
 menuClass.changePathImageOpenLogin(1);
+
+
+chargingClass.fillProgressBarProgressively(100);
--- assets/js/global/charging.js
+++ assets/js/global/charging.js
@@ -1,21 +1,26 @@
+class Charging {
+  constructor() {
 
+  }
+  fillProgressBarProgressively(finalPercentage) {
+      let currentPercentage = 0;
+      const interval = setInterval(() => {
+          currentPercentage++;
+          progressBarCharging.style.width = currentPercentage + '%';
+
+          if (currentPercentage >= finalPercentage) {
+              clearInterval(interval);
+          }
+      }, 2); // Adjust the timing here to control the speed
+  }
+}
 
 
 
 var progressBarCharging = document.getElementById("progressBarCharging");
+const chargingClass = new Charging();
 
 
-function fillProgressBarProgressively(finalPercentage) {
-    let currentPercentage = 0;
-    const interval = setInterval(() => {
-        currentPercentage++;
-        progressBarCharging.style.width = currentPercentage + '%';
-
-        if (currentPercentage >= finalPercentage) {
-            clearInterval(interval);
-        }
-    }, 2); // Adjust the timing here to control the speed
-}
 
 // Call the function
-fillProgressBarProgressively(100); // Fills the bar up to 100%
+//fillProgressBarProgressively(100); // Fills the bar up to 100%
