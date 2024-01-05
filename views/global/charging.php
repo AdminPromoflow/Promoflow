4ef0d67135115e55ea188f94118f448becf1fcb3
aelnajard <aelnajard@gmail.com>
2024-01-04 20:50:38 -0500

    “…”

--- views/global/charging.php
+++ views/global/charging.php
@@ -12,12 +12,15 @@
   left: 0px;
   background-color: rgba(0, 0, 0, .5);
 
-  
+
 }
 .iconCharging{
-  position: relative;
+  position: absolute;
   height: 80px;
   width: 120px;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
   background-color: rgba(255, 255, 255, 1);
 }
 .iconCharging img{
