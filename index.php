49bf4cc894237457ea466ae2387dff46f01e94e3
Aleina Rossui <admin@promoflow.net>
2025-07-08 04:51:12 -0700

    “…”

--- index.php
+++ index.php
@@ -6,9 +6,8 @@
   </head>
   <body>
     <script type="text/javascript">
-    //window.open("views/login/index.php", "_self");
+    window.open("views/login/index.php", "_self");
 
     </script>
-    <h1>hola</h1>
   </body>
 </html>
